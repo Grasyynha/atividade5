74fa58ef009fbba282572ba13f2cbfc6d43e716e
Grasiela Texeira <grasy.mst@gmail.com>
2024-04-29 23:49:57 -0300

    Instalação da biblioteca Faker

--- cypress/e2e/atividade5.cy.js
+++ cypress/e2e/atividade5.cy.js
@@ -1,5 +1,25 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+import { faker } from '@faker-js/faker';
+
+describe('Consulta de Usuários', () => {
+   beforeEach(function () {
+    cy.visit('https://rarocrud-frontend-88984f6e4454.herokuapp.com/users');
+
+    cy.contains('Novo').click();
+
+   })
+describe('Cadastro de Usuários', function() {
+        
+          const name = faker.person.fullName(); 
+          const email = faker.internet.email();  
+
+    it('Deve permitir o cadastro do usuario', function () {
+
+        
+          cy.get('#name').type(name); 
+          cy.get('#email').type(email); 
+          cy.get('.sc-dAlyuH').click(); 
+      
+          
+        });
+    });
 })
